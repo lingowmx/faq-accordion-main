34bb1b25b2ab5cb3f89c255d27d34860963c17d0
lingowmx <clingow@hotmail.com>
2025-03-19 12:42:40 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { BgImage } from './components/BgImage'
+
+function App() {
+
+
+  return (
+    <main className='font-worksans bg-lightPink h-screen w-screen flex justify-center'>
+      <BgImage/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/BgImage.tsx
@@ -0,0 +1,12 @@
+import mobileImage from '../../assets/images/background-pattern-mobile.svg'
+import desktopImage from '../../assets/images/background-pattern-desktop.svg'
+
+export const BgImage = () => {
+  return (
+    <picture>
+      <source srcSet={desktopImage} media='(min-width: 640px)'/>
+      <source srcSet={mobileImage} media='(max-width: 639px)'/>
+      <img src={mobileImage} alt="mobileImageBackground" />
+    </picture>
+  )
+}
